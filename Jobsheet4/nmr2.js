84fd403b9a7692e7c24b5be907cd6d9f6bc504ff
FakhriTmm <fakhritamimgt@gmail.com>
2023-08-09 07:10:16 +0700

    Jobsheet4 ajahh

---
+++ Jobsheet4/nmr2.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Masukkan sebuah kalimat: ", (inputString) => {
+    //Contoh property length
+    console.log(`Panjang Kalimat: ${inputString.length}`);
+    rl.close();
+});
